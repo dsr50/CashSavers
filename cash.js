2478c97199fa52650f6168f20f9fff90d3e312d7
dsr50 <butech01@outlook.com>
2014-02-19 02:17:16 -0600

    New customer site, and updates to all files

--- cash.js
+++ cash.js
@@ -1,5 +1,16 @@
-document.getElementById("mem").onMouseOver=function(){displayDate()};
-function displayDate() 
-{
-	document.getElementById("demo").innerHTML=Date();
-	}
+// Load welcome Message
+function welcomeMsg () {
+	var wel;
+	var c=confirm('Welcome to Cash Savers! Would you like to see our ad for this week?');
+ if (c == false) {
+	alert("Then we would load the html file.");
+}
+else if (c == true) {
+	alert("Good job, they answered yes!");
+}	
+else {
+	alert("This didn't work, bub.");
+}
+
+}
+
